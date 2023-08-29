4de688999d1f6f22262279b8defd73808c160c73
Marc Beinder <marc@marcbeinder.com>
2023-08-28 22:56:48 -0500

    People Full Support

---
+++ src/SDK/People/Person.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EncoreDigitalGroup\Laravel\PlanningCenter\SDK\Person;
+
+class Person
+{
+    public function all($PCOClient, $query = [])
+    {
+        $Person = new \EncoreDigitalGroup\PlanningCenter\People\Person;
+
+        return $Person->all($PCOClient, $query);
+    }
+
+    public function get($PCOClient, $id, $query = [])
+    {
+        $Person = new \EncoreDigitalGroup\PlanningCenter\People\Person;
+
+        return $Person->get($PCOClient, $id, $query);
+    }
+}
